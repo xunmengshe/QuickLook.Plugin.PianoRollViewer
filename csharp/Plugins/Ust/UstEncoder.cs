dc7dac587674a6069c938a4bdef00ca9ae244711
unknown <1463567152@qq.com>
2025-07-04 16:13:50 +0800

    support ust writing

--- csharp/Plugins/Ust/UstEncoder.cs
+++ csharp/Plugins/Ust/UstEncoder.cs
@@ -59,18 +59,66 @@ namespace OxygenDioxide.UstPlugin.Stream
             return (result, project.SongTempoList);
         }
 
-        public void EncodeFile(Project project, string filePath)
+        static List<UstNote> NotesToUstNotes(IEnumerable<Note> notes)
+        {
+            var ustNotes = new List<UstNote>();
+            var position = 0;
+            foreach (var note in notes)
+            {
+                if (note.StartPos < position)
+                {
+                    continue;
+                }
+                if (note.StartPos > position)
+                {
+                    ustNotes.Add(new UstNote()
+                    {
+                        position = position,
+                        duration = note.StartPos - position,
+                        lyric = "R",
+                        noteNum = 60,
+                    });
+                }
+                ustNotes.Add(new UstNote()
+                {
+                    position = note.StartPos,
+                    duration = note.Length,
+                    lyric = note.Lyric,
+                    noteNum = note.KeyNumber
+                });
+                position = note.StartPos + note.Length;
+            }
+            // TODO: Insert tempo changes.
+            return ustNotes;
+        }
+
+        public void SaveFile(Project project, string filePath)
         {
             (var notes, var tempos) = PrepareProject(project);
+            var ustNotes = NotesToUstNotes(notes);
             using (var writer = new StreamWriter(filePath, false, ShiftJIS))
             {
-
+                WriteHeader(project, writer);
+                for (var i = 0; i < ustNotes.Count; i++)
+                {
+                    writer.WriteLine($"[#{i:D4}]");
+                    ustNotes[i].Write(writer);
+                }
+                WriteFooter(writer);
             }
         }
-        
+
         static void WriteHeader(Project project, StreamWriter writer)
         {
-            
+            writer.WriteLine("[#SETTING]");
+            writer.WriteLine($"Tempo={project.SongTempoList[0].BPM}");
+            writer.WriteLine("Tracks=1");
+            writer.WriteLine("Mode2=True");
+        }
+        
+        static void WriteFooter(StreamWriter writer)
+        {
+            writer.WriteLine("[#TRACKEND]");
         }
     }
 }
